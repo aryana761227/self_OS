43dfffeec08cc5d45303068b572c8fd53622e1c5
aryana <aryana761227@gmail.com>
2019-01-08 15:53:49 +0330

    interrupt handler implemented which link interruptmanager and keyboard and keyboard implemented too completley(not ascci code)

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "gdt.h"
 #include "interrupts.h"
+#include "keyboard.h"
 
 void printf(char* str)
 {
@@ -55,8 +56,8 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
 {
     printf("Hello World! --- http://www.AlgorithMan.de");
     GlobalDescriptorTable gdt;
-
     InterruptManager interrupts(0x20, &gdt);
+    KeyboardDriver keyboard(&interrupts);
     interrupts.Activate();
 
     while(1);
